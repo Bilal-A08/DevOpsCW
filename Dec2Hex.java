466328b0df3e3e5d38e1a0bc6c302272c6299397
Bilal-A08 <bltcstudio@gmail.com>
2024-11-06 19:47:59 +0000

    Task 6 - fix issues highlighted by sonarqube for Dec2Hex.java file

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,27 +1,32 @@
-import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 class Dec2Hex
 {
-    public static int Arg1;
-    public static void main(String args[])    {
+    public static int arg1;
+	
+    private static final Logger logger = Logger.getLogger(Dec2Hex.class.getName());
+
+    public static void main(String[] args)    {
 	//bilal's comment - check for missing input
 	if(args.length == 0){
-	   System.out.println("Error: No input provided. Please provide a valid number");
+	   logger.log(Level.INFO, "Error: No input provided. Please provide a valid number");
+	   return;
 	}
 
 	//bilal's comment - validate that the input is an integer
 	try{
-	   Arg1 = Integer.parseInt(args[0]);
+	   arg1 = Integer.parseInt(args[0]);
 	} catch(NumberFormatException e){
-	   System.out.println("Error: Failed to convert non-integer input");
+	   logger.log(Level.INFO, "Error: Failed to convert non-integer input");
 	   return;
 	}
 
-	char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+	char[] ch={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
 	int rem, num;
-        num = Arg1;
+        num = arg1;
         String hexadecimal="";
-        System.out.println("Converting the Decimal Value " + num + " to Hex...");
+        logger.log(Level.INFO, "Converting the Decimal Value " + num + " to Hex...");
 
 
         while(num != 0)
@@ -31,7 +36,6 @@ class Dec2Hex
             num= num/16;
         }
 
-        System.out.println("Hexadecimal representation is: " + hexadecimal);
-
+        logger.log(Level.INFO, "Hexadecimal representation is: " + hexadecimal);
     }
 }
