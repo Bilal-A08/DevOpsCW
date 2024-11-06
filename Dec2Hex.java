60600765e049e1d815b14451c7db58dfb31eabbd
Bilal-A08 <bltcstudio@gmail.com>
2024-11-06 13:59:06 +0000

    Task 5 - add check for missing input argument and return error message

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -5,6 +5,11 @@ class Dec2Hex
 public static int Arg1;
     public static void main(String args[])    {
 
+	//bilal's comment - check for missing input
+	if(args.length == 0){
+	   System.out.println("Error: No input provided. Please provide a valid number");
+	}
+
         Arg1 = Integer.parseInt(args[0]);
         char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         int rem, num;
