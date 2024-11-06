488c9af47497316748286449a84fdebc0a549f8a
Bilal-A08 <bltcstudio@gmail.com>
2024-11-06 14:16:04 +0000

    Task 5 - add try/catch statement for error handling when processing non-integer input

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -2,20 +2,27 @@ import java.util.Scanner;
 
 class Dec2Hex
 {
-public static int Arg1;
+    public static int Arg1;
     public static void main(String args[])    {
-
 	//bilal's comment - check for missing input
 	if(args.length == 0){
 	   System.out.println("Error: No input provided. Please provide a valid number");
 	}
 
-        Arg1 = Integer.parseInt(args[0]);
-        char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-        int rem, num;
+	//bilal's comment - validate that the input is an integer
+	try{
+	   Arg1 = Integer.parseInt(args[0]);
+	} catch(NumberFormatException e){
+	   System.out.println("Error: Failed to convert non-integer input");
+	   return;
+	}
+
+	char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+	int rem, num;
         num = Arg1;
         String hexadecimal="";
         System.out.println("Converting the Decimal Value " + num + " to Hex...");
+
 
         while(num != 0)
         {
