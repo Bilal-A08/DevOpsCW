e78b8855704ab48a3044bbd54ead0722bd4a4a0c
Bilal-A08 <bltcstudio@gmail.com>
2024-11-07 17:04:37 +0000

    Task 6 - fix issues highlighted by sonarqube for TestRunner.java, remove unused import

--- TestRunner.java
+++ TestRunner.java
@@ -2,7 +2,6 @@ import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 
 import org.junit.runner.notification.Failure;
-import java.lang.Exception;
 
 public class TestRunner {
    public static void main(String[] args) {
