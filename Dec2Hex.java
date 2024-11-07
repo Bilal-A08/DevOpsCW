b3fc79986eaa68fcecd9aa800967331a70f5b63d
Bilal-A08 <bltcstudio@gmail.com>
2024-11-07 22:40:34 +0000

    Practical Evidence - Jenkins Console Ouput - testing with original code

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,38 +1,24 @@
+import java.util.Scanner;
+
 class Dec2Hex
 {
-    public static void main(String[] args)    {
-	//bilal's comment - made arg1 non static since it's only being used here
-	int arg1;
-
-	//bilal's comment - check for missing input
-	if(args.length == 0){
-	   System.out.println("Error: No input provided. Please provide a valid number");
-	   return;
-	}
-
-	//bilal's comment - validate that the input is an integer
-	try{
-	   arg1 = Integer.parseInt(args[0]);
-	} catch(NumberFormatException e){
-	   System.out.println("Error: Failed to convert non-integer input");
-	   return;
-	}
-
-	char[] ch={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-	int rem;
-	int num;
-        num = arg1;
-        StringBuilder hexadecimal = new StringBuilder(); //bilal's comment - use StringBuilder instead of String, highlighted by sonarqube
+public static int Arg1;
+    public static void main(String args[])    {
+        Arg1 = Integer.parseInt(args[0]);
+        char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+        int rem, num;
+        num = Arg1;
+        String hexadecimal="";
         System.out.println("Converting the Decimal Value " + num + " to Hex...");
-
 
         while(num != 0)
         {
             rem=num%16;
-            hexadecimal.insert(0, ch[rem]);
+            hexadecimal= ch[rem] + hexadecimal;
             num= num/16;
         }
 
-        System.out.println("Hexadecimal representation is: " + hexadecimal.toString());
+        System.out.println("Hexadecimal representation is: " + hexadecimal);
+
     }
 }
