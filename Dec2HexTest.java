e5c69c8d87b2393f06f4a3e1beed85b933186201
Bilal-A08 <bltcstudio@gmail.com>
2024-11-07 22:28:23 +0000

    add comments to Dec2HexTest.java file

--- Dec2HexTest.java
+++ Dec2HexTest.java
@@ -9,20 +9,29 @@ public class Dec2HexTest{
 	}
 
 	private String captureOutput(String[] args){
+	   //create a stream to capture the printed output
 	   ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+	   //save the original System.out to restore
 	   PrintStream originalOut = System.out;
+
+	   //redirect System.out to the custom output stream
 	   System.setOut(new PrintStream(outContent));
 
 	   try {
+		//call the main method of Dec2Hex with provided arguments
 		Dec2Hex.main(args);
 	   } finally {
+		//restore the original System.out after saving the output
 		System.setOut(originalOut);
 	   }
 
+	   //return the captured output
 	   return outContent.toString().trim();
 	}
 
 
+	//test case checks for a valid decimal to hexadecimal conversion
 	@Test
 	public void testValidDecimalToHex(){
 	   String output = captureOutput(new String[]{"11"});
@@ -30,6 +39,7 @@ public class Dec2HexTest{
 	   assertEquals(expectedOutput, output);
 	}
 
+	//test case checks for a non integer argument
 	@Test
 	public void testNonIntegerArgument(){
 	   String output = captureOutput(new String[]{"B"});
